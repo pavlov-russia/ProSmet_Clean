c6d43496545631b63be77f4cec6a4b6d42c04a29
Alex <mr-pas@mail.ru>
2026-05-23 10:57:05 +0200

    Configure development tools

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      ".next/**",
+      "coverage/**",
+      ".vscode-server/**"
+    ],
+  },
+];
